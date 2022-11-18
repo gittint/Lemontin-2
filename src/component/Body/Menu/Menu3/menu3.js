e4c14af959580ff05390ccb1ed0fd9ae335e97c9
Dngtint-Loptin <jimindngtint@gmail.com>
2022-11-18 12:37:35 +0700

    Menu ...

--- src/component/Body/Menu/Menu3/menu3.js
+++ src/component/Body/Menu/Menu3/menu3.js
@@ -2,16 +2,62 @@ import MenuHead from '../menuHead'
 import '../baseMenuComponent.css'
 import axios from 'axios'
 import {useEffect, useState} from "react"
+import MenuItems from '../menuItem'
 
 function Menu3 (){
+    const [datas,setDatas]  = useState([])
+    const fetchAPI = async () => {
+        const getAPI = await axios.get('https://6373003c0bb6b698b6ffe030.mockapi.io/menu')
+        let res = await getAPI.data;
+        setDatas(res[0].menu3)
+    }
+    useEffect(  () => {
+        fetchAPI();
+    },[])
+
     return(
         <div className="grid">
             <MenuHead/>
+            
+            {/* <!-- base sub menu --> */}
             <div class="app-container__content menu-app-container__content">
-                <div class="menu5-content__body">
-                    <div class="menu5-content__body-header">Hiện tại chưa có thực đơn mới</div>
+                <div class="sub-menu-base">
+                    <div class="sub-menu-base--list">
+                        {
+                            datas.map((menu3,index)=>(
+                                <a href={"#"+ menu3.id} key={index} class="sub-menu-base--item">{menu3.type}</a>
+                            ))
+                        }
+                    </div>
+                    
                 </div>
+                
             </div>
+
+            {/* <!-- base menu FAD  --> */}
+            <div class="app-container__content menu-app-container__content">
+                <div class="fad-menu-base">
+                    <ul class="fad-menu-base--list">
+                        {
+                            datas.map((menu3,index)=>(
+                                <li class="fad-menu-base--item" id={menu3.id}>
+                                    <div class="fad-menu-base--img-box">
+                                        <div class="fad-menu-base--img-label">{menu3.type}</div>
+                                        <img src={menu3.img} alt="" class="fad-menu-base--img"/>
+                                    </div>
+
+                                    <div class="fad-menu-base--content-box">
+                                        <MenuItems list={menu3.menu}/>
+                                    </div>
+                                </li>
+                            ))
+                        }
+
+                    </ul>                        
+                </div>
+                    
+            </div>
+
         </div>
     )
 }
